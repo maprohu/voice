f7df92911c53a69c9294e7a5cb0ecec545a26183
maprohu <maprohu@gmail.com>
2016-09-25 10:56:19 +0100

    coursera

--- sandbox/src/main/scala/voice/sandbox/AudioTools.scala
+++ sandbox/src/main/scala/voice/sandbox/AudioTools.scala
@@ -1,9 +1,10 @@
 package voice.sandbox
 
-import java.io.{File, FileInputStream, FileOutputStream}
+import java.io._
 import javax.sound.sampled.{AudioFormat, AudioSystem}
 
 import akka.util.ByteString
+import monix.reactive.Observable
 
 import scala.concurrent.Promise
 
@@ -14,51 +15,76 @@ object AudioTools {
 
   val Format = new AudioFormat(44100.0f, 16, 1, true, true)
 
+  val MixerInfo =
+    AudioSystem
+      .getMixerInfo
+      .find(_.getName.toLowerCase.contains("microphone"))
+      .get
+
   def record(file: File) : () => Unit = {
     file.getParentFile.mkdirs()
     val out = new FileOutputStream(file)
-    val line = AudioSystem.getTargetDataLine(Format)
+    record(out)
+  }
+
+  def record(out: OutputStream) : () => Unit = {
+    listen(
+      { buffer =>
+        { numRead =>
+          if (numRead == 0) out.close()
+          else out.write(buffer, 0, numRead)
+        }
+      }
+    )
+  }
+
+  type Buffer = Array[Byte]
+
+  def listen(
+    starter : Buffer => (Int => Unit)
+  ) : () => Unit = {
+    val line = AudioSystem.getTargetDataLine(Format, MixerInfo)
 
     line.open(Format)
     line.start()
 
+    val buffer = Array.ofDim[Byte](line.getBufferSize / 5)
 
-//    val finish = Promise[Unit]()
+    val chunk = starter(buffer)
 
     new Thread() {
       override def run(): Unit = {
-        val buffer = Array.ofDim[Byte](line.getBufferSize / 5)
-
-        Iterator
-          .continually(
-            line.read(buffer, 0, buffer.length)
-          )
-          .takeWhile(_ != 0)
-          .foreach({ numRead =>
-            out.write(buffer, 0, numRead)
-          })
-
-//        while (!finish.isCompleted) {
-//          val numRead = line.read(buffer, 0, buffer.length)
-//          out.write(buffer, 0, numRead)
-//        }
-
-        out.close()
+        var numRead = 0
+        do {
+          numRead = line.read(buffer, 0, buffer.length)
+          chunk(numRead)
+        } while (numRead != 0)
       }
     }.start()
 
-    { () =>
+    val stopper = { () =>
       line.drain()
       line.stop()
       line.close()
-//      finish.success()
     }
+
+    stopper
   }
 
-  def play(file: File) = {
+
+  def play(file: File) : Unit = {
+    val in = new FileInputStream(file)
+    play(in)
+  }
+
+  val Noop = () => ()
+
+  def play(
+    in: InputStream,
+    stopper: () => Unit = Noop
+  ) : Unit = {
     new Thread() {
       override def run(): Unit = {
-        val in = new FileInputStream(file)
 
         val line = AudioSystem.getSourceDataLine(Format)
         line.open(Format)
@@ -82,6 +108,8 @@ object AudioTools {
         line.drain()
         line.stop()
         line.close()
+
+        stopper()
       }
     }.start()
   }
