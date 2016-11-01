e232ae08557b705c37c1cb07e19a708c25838480
maprohu <maprohu@gmail.com>
2016-11-01 21:17:34 +0000

    bluet

--- modules/src/main/scala/voice/modules/VoiceRpiModules.scala
+++ modules/src/main/scala/voice/modules/VoiceRpiModules.scala
@@ -4,7 +4,7 @@ import maven.modules.builder.{RootModuleContainer, ScalaModule, SubModuleContain
 import maven.modules.utils.MavenCentralModule
 import toolbox6.modules.Toolbox6Modules.Logging
 import toolbox6.modules.{JarTreeModules, Toolbox6Modules, UiModules}
-import toolbox8.modules.{JarTree8Modules, RpiModules, Toolbox8Modules}
+import toolbox8.modules.{Extra8Modules, JarTree8Modules, RpiModules, Toolbox8Modules}
 
 /**
   * Created by martonpapp on 29/08/16.
@@ -28,7 +28,9 @@ object VoiceRpiModules {
     Core,
     JarTree8Modules.Util,
     Toolbox6Modules.Logging,
-    JarTreeModules.Util
+    JarTreeModules.Util,
+    Extra8Modules.Server,
+    JarTreeModules.Impl
   )
 
   object Mobile extends ScalaModule(
--- rpi/home/src/main/scala/voice/rpi/home/VoiceHomePlugger.scala
+++ rpi/home/src/main/scala/voice/rpi/home/VoiceHomePlugger.scala
@@ -1,38 +1,33 @@
 package voice.rpi.home
 
-import java.io.FileInputStream
-import java.nio.file.{Files, Path, Paths}
-
-import akka.event.Logging
-import akka.stream.Attributes
 import akka.stream.scaladsl.{FileIO, Flow, Sink, Source, StreamConverters}
 import akka.util.ByteString
 import com.typesafe.scalalogging.LazyLogging
-import toolbox6.jartree.api.{JarPlugResponse, JarPlugger}
+import toolbox6.jartree.api.{JarCacheLike, JarPlugResponse, JarPlugger}
+import toolbox6.jartree.impl.JarTree
 import toolbox6.jartree.util.JarTreeTools
+import toolbox8.jartree.extra.server.ExecServer
 import toolbox8.jartree.standaloneapi.{JarTreeStandaloneContext, PeerInfo, Service}
 
-import scala.concurrent.Future
+import scala.concurrent.{ExecutionContext, Future}
 
-object VoiceHome extends Service {
+class VoiceHome(
+  cache: JarCacheLike
+)(implicit
+  executionContext: ExecutionContext
+) extends Service {
+  val jarTree = new JarTree(
+    classOf[VoiceHome].getClassLoader,
+    cache
+  )
+
   override def apply(info: PeerInfo): Future[Flow[ByteString, ByteString, _]] = {
     Future.successful(
-      Flow.fromSinkAndSource(
-        Sink.ignore,
-        Source.maybe
-//        StreamConverters
-//          .fromInputStream(
-//            () => new FileInputStream("/dev/hidraw0")
-////            3
-//          )
-////        FileIO
-////          .fromPath(
-////            Paths.get("/dev/hidraw0"),
-////            3
-////          )
-//          .log("hid").withAttributes(Attributes.logLevels(onFinish = Logging.InfoLevel, onFailure = Logging.InfoLevel))
-////          .log("hid")
-      )
+      ExecServer
+        .flow(
+          this,
+          jarTree
+        )
     )
   }
 
@@ -44,9 +39,11 @@ class VoiceHomePlugger
   with LazyLogging {
 
   override def pullAsync(previous: Service, context: JarTreeStandaloneContext): Future[JarPlugResponse[Service]] = {
+    import context._
+    import actorSystem.dispatcher
     Future.successful(
       JarTreeTools.andThenResponse(
-        VoiceHome,
+        new VoiceHome(context.jarCache),
         () => previous.close()
       )
     )
