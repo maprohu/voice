791aba643e8cd7fea005d459ec5ecd7b4fb9aa68
maprohu <maprohu@gmail.com>
2016-11-30 20:21:01 +0000

    voice central

---
+++ modules/src/test/scala/voice/modules/build.scala
@@ -0,0 +1,18 @@
+package voice.modules
+
+/**
+  * Created by maprohu on 30-11-2016.
+  */
+object build {
+
+
+}
+
+object build_voice_modules {
+  def main(args: Array[String]): Unit = {
+
+
+
+  }
+}
+
